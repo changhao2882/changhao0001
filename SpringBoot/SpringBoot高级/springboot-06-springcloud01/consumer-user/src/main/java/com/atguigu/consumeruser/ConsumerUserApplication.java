88db9b282e1157761a9815ce04f2847d1523b391
changhao2882 <changhao2882@163.com>
2020-05-03 21:54:21 +0800

    注释

---
+++ SpringBoot/SpringBoot高级/springboot-06-springcloud01/consumer-user/src/main/java/com/atguigu/consumeruser/ConsumerUserApplication.java
@@ -0,0 +1,24 @@
+package com.atguigu.consumeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient //开启发现服务功能
+@SpringBootApplication
+public class ConsumerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerUserApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced //使用负载均衡机制  各个提供者访问一次
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
